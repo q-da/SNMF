38e2c36adef8464853233a5fed5a86a4a80bada0
q-da <83254787+q-da@users.noreply.github.com>
2025-02-19 14:26:35 +0800

    Add files via upload

---
+++ zhu1/zhu_zhuang_tu.m
@@ -0,0 +1,195 @@
+clc;clear;
+%确定图片的位置和大小，[x y width height]
+figure('position',[150,100,750,650]);
+% %准备数据
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%yale
+%num=readmatrix('D:\graduate\matlab\NMF\DABNMTF-main\zhu\coil20.xlsx') ;
+num=readmatrix('D:\graduate\matlab\NMF\create\ENMF\zhu1\yale.xlsx') ;
+
+%画柱状图
+%axis([-inf inf -inf inf 60 100])
+b=bar3(num);
+for k = 1:length(b)
+    zdata = b(k).ZData;
+    b(k).CData = zdata;
+    b(k).FaceColor = 'interp';
+end
+%设置小标签
+set(gca,'yticklabel',{'3','5','7','9','11','13','15'},'Fontname','Times New Roman','FontSize',11);
+set(gca,'xticklabel',{'0','0.001','0.01','0.1','1','10','100'},'Fontname','Times New Roman','FontSize',11);
+%修改每个轴的标签
+%title('COIL20')
+title('YALE')
+colorbar
+ylabel('K','Fontname','Times New Roman','FontSize',14);
+xlabel('\lambda','Fontname','Times New Roman','FontSize',14);
+zlabel('ACC(%)','Fontname','Times New Roman','FontSize',14);
+%legend({'M_1','M_2','M_3','M_4'});
+figure('position',[150,100,750,650]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%coil20
+num=readmatrix('D:\graduate\matlab\NMF\create\ENMF\zhu1\coil20.xlsx') ;
+%num=readmatrix('D:\graduate\matlab\NMF\DABNMTF-main\zhu\yale.xlsx') ;
+
+%画柱状图
+%axis([-inf inf -inf inf 60 100])
+b=bar3(num);
+for k = 1:length(b)
+    zdata = b(k).ZData;
+    b(k).CData = zdata;
+    b(k).FaceColor = 'interp';
+end
+%设置小标签
+set(gca,'yticklabel',{'3','5','7','9','11','13','15'},'Fontname','Times New Roman','FontSize',11);
+set(gca,'xticklabel',{'0','0.001','0.01','0.1','1','10','100'},'Fontname','Times New Roman','FontSize',11);
+%修改每个轴的标签
+title('COIL20')
+%title('Yale')
+colorbar
+ylabel('K','Fontname','Times New Roman','FontSize',14);
+xlabel('\lambda','Fontname','Times New Roman','FontSize',14);
+zlabel('ACC(%)','Fontname','Times New Roman','FontSize',14);
+%legend({'M_1','M_2','M_3','M_4'});
+figure('position',[150,100,750,650]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%coil100
+%num=readmatrix('D:\graduate\matlab\NMF\DABNMTF-main\zhu\coil20.xlsx') ;
+num=readmatrix('D:\graduate\matlab\NMF\create\ENMF\zhu1\coil100.xlsx') ;
+
+%画柱状图
+%axis([-inf inf -inf inf 60 100])
+b=bar3(num);
+for k = 1:length(b)
+    zdata = b(k).ZData;
+    b(k).CData = zdata;
+    b(k).FaceColor = 'interp';
+end
+%设置小标签
+set(gca,'yticklabel',{'3','5','7','9','11','13','15'},'Fontname','Times New Roman','FontSize',11);
+set(gca,'xticklabel',{'0','0.001','0.01','0.1','1','10','100'},'Fontname','Times New Roman','FontSize',11);
+%修改每个轴的标签
+%title('COIL20')
+title('COIL100')
+colorbar
+ylabel('K','Fontname','Times New Roman','FontSize',14);
+xlabel('\lambda','Fontname','Times New Roman','FontSize',14);
+zlabel('ACC(%)','Fontname','Times New Roman','FontSize',14);
+%legend({'M_1','M_2','M_3','M_4'});
+figure('position',[150,100,750,650]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%FERET
+%num=readmatrix('D:\graduate\matlab\NMF\DABNMTF-main\zhu\coil20.xlsx') ;
+num=readmatrix('D:\graduate\matlab\NMF\create\ENMF\zhu1\FERET.xlsx') ;
+
+%画柱状图
+%axis([-inf inf -inf inf 60 100])
+b=bar3(num);
+for k = 1:length(b)
+    zdata = b(k).ZData;
+    b(k).CData = zdata;
+    b(k).FaceColor = 'interp';
+end
+%设置小标签
+set(gca,'yticklabel',{'3','5','7','9','11','13','15'},'Fontname','Times New Roman','FontSize',11);
+set(gca,'xticklabel',{'0','0.001','0.01','0.1','1','10','100'},'Fontname','Times New Roman','FontSize',11);
+%修改每个轴的标签
+%title('COIL20')
+title('FERET')
+colorbar
+ylabel('K','Fontname','Times New Roman','FontSize',14);
+xlabel('\lambda','Fontname','Times New Roman','FontSize',14);
+zlabel('ACC(%)','Fontname','Times New Roman','FontSize',14);
+%legend({'M_1','M_2','M_3','M_4'});
+figure('position',[150,100,750,650]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%GT
+%num=readmatrix('D:\graduate\matlab\NMF\DABNMTF-main\zhu\coil20.xlsx') ;
+num=readmatrix('D:\graduate\matlab\NMF\create\ENMF\zhu1\GT.xlsx') ;
+
+%画柱状图
+%axis([-inf inf -inf inf 60 100])
+b=bar3(num);
+for k = 1:length(b)
+    zdata = b(k).ZData;
+    b(k).CData = zdata;
+    b(k).FaceColor = 'interp';
+end
+%设置小标签
+set(gca,'yticklabel',{'3','5','7','9','11','13','15'},'Fontname','Times New Roman','FontSize',11);
+set(gca,'xticklabel',{'0','0.001','0.01','0.1','1','10','100'},'Fontname','Times New Roman','FontSize',11);
+%修改每个轴的标签
+%title('COIL20')
+title('GT')
+colorbar
+ylabel('K','Fontname','Times New Roman','FontSize',14);
+xlabel('\lambda','Fontname','Times New Roman','FontSize',14);
+zlabel('ACC(%)','Fontname','Times New Roman','FontSize',14);
+%legend({'M_1','M_2','M_3','M_4'});
+figure('position',[150,100,750,650]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%ORL
+%num=readmatrix('D:\graduate\matlab\NMF\DABNMTF-main\zhu\coil20.xlsx') ;
+num=readmatrix('D:\graduate\matlab\NMF\create\ENMF\zhu1\ORL.xlsx') ;
+
+%画柱状图
+%axis([-inf inf -inf inf 60 100])
+b=bar3(num);
+for k = 1:length(b)
+    zdata = b(k).ZData;
+    b(k).CData = zdata;
+    b(k).FaceColor = 'interp';
+end
+%设置小标签
+set(gca,'yticklabel',{'3','5','7','9','11','13','15'},'Fontname','Times New Roman','FontSize',11);
+set(gca,'xticklabel',{'0','0.001','0.01','0.1','1','10','100'},'Fontname','Times New Roman','FontSize',11);
+%修改每个轴的标签
+%title('COIL20')
+title('ORL')
+colorbar
+ylabel('K','Fontname','Times New Roman','FontSize',14);
+xlabel('\lambda','Fontname','Times New Roman','FontSize',14);
+zlabel('ACC(%)','Fontname','Times New Roman','FontSize',14);
+%legend({'M_1','M_2','M_3','M_4'});
+figure('position',[150,100,750,650]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%PIE
+%num=readmatrix('D:\graduate\matlab\NMF\DABNMTF-main\zhu\coil20.xlsx') ;
+num=readmatrix('D:\graduate\matlab\NMF\create\ENMF\zhu1\PIE.xlsx') ;
+
+%画柱状图
+%axis([-inf inf -inf inf 60 100])
+b=bar3(num);
+for k = 1:length(b)
+    zdata = b(k).ZData;
+    b(k).CData = zdata;
+    b(k).FaceColor = 'interp';
+end
+%设置小标签
+set(gca,'yticklabel',{'3','5','7','9','11','13','15'},'Fontname','Times New Roman','FontSize',11);
+set(gca,'xticklabel',{'0','0.001','0.01','0.1','1','10','100'},'Fontname','Times New Roman','FontSize',11);
+%修改每个轴的标签
+%title('COIL20')
+title('PIE')
+colorbar
+ylabel('K','Fontname','Times New Roman','FontSize',14);
+xlabel('\lambda','Fontname','Times New Roman','FontSize',14);
+zlabel('ACC(%)','Fontname','Times New Roman','FontSize',14);
+%legend({'M_1','M_2','M_3','M_4'});
+figure('position',[150,100,750,650]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Umist
+%num=readmatrix('D:\graduate\matlab\NMF\DABNMTF-main\zhu\coil20.xlsx') ;
+num=readmatrix('D:\graduate\matlab\NMF\create\ENMF\zhu1\Umist.xlsx') ;
+
+%画柱状图
+%axis([-inf inf -inf inf 60 100])
+b=bar3(num);
+for k = 1:length(b)
+    zdata = b(k).ZData;
+    b(k).CData = zdata;
+    b(k).FaceColor = 'interp';
+end
+%设置小标签
+set(gca,'yticklabel',{'3','5','7','9','11','13','15'},'Fontname','Times New Roman','FontSize',11);
+set(gca,'xticklabel',{'0','0.001','0.01','0.1','1','10','100'},'Fontname','Times New Roman','FontSize',11);
+%修改每个轴的标签
+%title('COIL20')
+title('UMIST')
+colorbar
+ylabel('K','Fontname','Times New Roman','FontSize',14);
+xlabel('\lambda','Fontname','Times New Roman','FontSize',14);
+zlabel('ACC(%)','Fontname','Times New Roman','FontSize',14);
+%legend({'M_1','M_2','M_3','M_4'});
